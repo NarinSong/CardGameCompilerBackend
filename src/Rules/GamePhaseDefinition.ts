a7073a5aa0b462e592af362841ea5eec5b25f829
Omar-Alqawasmi <ona6594@mavs.uta.edu>
2026-03-14 00:17:52 -0500

    Documentation for GamePhaseDefinition.ts

--- src/Rules/GamePhaseDefinition.ts
+++ src/Rules/GamePhaseDefinition.ts
@@ -6,15 +6,30 @@
 import LabelManager, { PhaseLabel } from "./LabelManager";
 import StepDefinition from "./StepDefinition";
 
+/**
+ * Defines the properties for a phase in the game.
+ * 
+ * A GamePhaseDefinition contains the steps that occur in a phase and the label to identify that phase.
+ */
 export default class GamePhaseDefinition {
     steps: StepDefinition[];
     label: PhaseLabel;
 
+    /**
+     * Creates a new game phase definition.
+     * @param labelManager - Label manager to create the phase label.
+     * @param name - Optional name for the phase.
+     * @param steps - Optional steps for the phase.
+     */
     constructor(labelManager: LabelManager, name?: string, steps?: StepDefinition[]) {
         this.label = labelManager.createPhaseLabel(this, name);
         this.steps = steps || [];
     }
 
+    /**
+     * Adds a step to the phase.
+     * @param step - The step to add to the phase.
+     */
     addStep(step: StepDefinition) {
         this.steps.push(step);
     }
