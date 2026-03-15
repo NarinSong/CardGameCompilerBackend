4ff86b5cd2243aa331cff9f6cb847c0e6bf360b3
Omar-Alqawasmi <ona6594@mavs.uta.edu>
2026-03-15 05:47:38 -0500

    Documentation for Pile.ts

--- src/Game/Pile.ts
+++ src/Game/Pile.ts
@@ -4,6 +4,11 @@ import PileDefinition from "../Rules/PileDefinition";
 import { ActionRole, DisplayName, PileState, Visibility } from "../schemas/types";
 import GameLabels from "./GameLabels";
 
+/**
+ * Defines a Pile of cards in the card.
+ * 
+ * A Pile stores its cards, label, visibility, action roles, and display name.
+ */
 export default class Pile {
     cards: Card[];
     label: Label;
@@ -11,6 +16,18 @@ export default class Pile {
     actionRoles: ActionRole[];
     displayName: DisplayName;
 
+    /**
+     * Creates a new pile.
+     * 
+     * This constructor is private and should only be called through the provided factory methods.
+     * 
+     * @param initialState - The initial configuration of the pile (e.g., "SORTED", "EMPTY", "SHUFFLED").
+     * @param label - The unique name given to the pile.
+     * @param visibility - The visibility of the pile.
+     * @param gameLabels - The game's label manager.
+     * @param actionRoles - The actions that can be done with the pile.
+     * @param displayName - The display name shown in the game.
+     */
     private constructor(
         initialState: PileState,
         label: Label,
@@ -28,6 +45,12 @@ export default class Pile {
         gameLabels.registerPile(this, this.label);
     }
 
+    /**
+     * Creates a pile from PileDefinition.
+     * @param definition - Configuration for the pile, including its label, display name, action roles, initial state, and visibility.
+     * @param gameLabels - The game's label manager.
+     * @returns A newly constructed pile.
+     */
     static fromDefinition(definition: PileDefinition, gameLabels: GameLabels) {
         return new Pile(
             definition.initialState,
@@ -39,6 +62,16 @@ export default class Pile {
         );
     }
 
+    /**
+     * Creates a new pile using explicit parameters.
+     * @param initialState - The initial configuration of the pile (e.g., ""SORTED", "EMPTY", "SHUFFLED").
+     * @param label - The unique name given to the pile.
+     * @param visibility - The visibility of the pile.
+     * @param gameLabels - The game's label manager.
+     * @param actionRoles - The actions that can be done with the pile.
+     * @param displayName - The display name shown in the game.
+     * @returns A newly constructed pile.
+     */
     static create(
         initialState: PileState,
         label: Label,
