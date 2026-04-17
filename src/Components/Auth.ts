c197ad8b34635e0d0f6ef9649b2824eb0e2758d6
Omar-Alqawasmi <ona6594@mavs.uta.edu>
2026-04-17 02:29:09 -0500

    Docs for Auth.ts

--- src/Components/Auth.ts
+++ src/Components/Auth.ts
@@ -5,12 +5,27 @@ import Logger from "./Logger.js";
 
 const ACTIVE_USERS: Record<string, string> = {}; // token, username
 
+/**
+ * function to log any reason for failure
+ * @param reason - reason for failure.
+ * @returns null
+ */
 function failureReason(reason: string): null {
     Logger.log(reason);
     return null;
 }
 
+/**
+ *  Used to authenticate users.
+ */
 export default class Auth {
+
+    /**
+     * Verify that user trying to reconnect is already signed in.
+     * @param username - name of the user trying to reconnect.
+     * @param token - session token.
+     * @returns true if token matches username, null if the token is invalid.
+     */
     // all usernames are in lowercase before arriving here. Those are sanitized in the ClientRequestParser
     // verifyUser is for a client who is already signed in and just wants to reconnect
     static verifyUser(username: string, token: string) {
@@ -22,6 +37,10 @@ export default class Auth {
         return account == username;
     }
 
+    /**
+     * Asynchronous random bytes generator.
+     * @returns a Promise resolving to a buffer of 128 random bytes, or null on failure.
+     */
     // Despite NonSharedBuffer being deprecated, it's what the randomBytes built-in function returns, so it's what we're using
     static async randomBytes(): Promise<NonSharedBuffer | null> {
         // Have to wrap the callback-based randomBytes function in a promise
@@ -37,6 +56,12 @@ export default class Auth {
         });
     }
 
+    /**
+     * Authenticates user if username and password combination is correct and exists in the database.
+     * @param username - username of the user.
+     * @param password - password that user inputs.
+     * @returns an object with a token and display name of the user.
+     */
     static async authenticateUser(username: string, password: string): Promise<{token: string, displayName: string} | null> {
         const passwordHashArray = await Database.getHashByUsername(username);
         
@@ -56,6 +81,13 @@ export default class Auth {
         return {token: sessionId, displayName: passwordHashArray[0].displayName};
     }
 
+    /**
+     * Creates a new user.
+     * @param username - username of the new user.
+     * @param password - password for the new user.
+     * @param displayName - display name for the new user.
+     * @returns a session id if successful else null if fails.
+     */
     static async createNewUser(username: string, password: string, displayName: string): Promise<string | null> {
         const passwordHashArray = await Database.getHashByUsername(username);
         
@@ -82,12 +114,22 @@ export default class Auth {
         return sessionId;
     }
 
+    /**
+     * Signs the user out.
+     * @param token - token of the user to be signed out.
+     * @returns true if successful, or false if no active user has this token.
+     */
     static async signOut(token: string): Promise<boolean> {
         if (!ACTIVE_USERS[token]) return false;
         delete ACTIVE_USERS[token];
         return true;
     }
 
+    /**
+     * Signs out a user given their username in all sessions.
+     * @param username - username of the user signing out.
+     * @returns true if user found, false if not.
+     */
     static async signOutEverywhere(username: string): Promise<boolean> {
         let found = false;
 
