a8e7433aced372e9d912f56ad54aee42f243e361
Omar-Alqawasmi <ona6594@mavs.uta.edu>
2026-04-17 00:50:11 -0500

    Documentation for Client.ts

--- src/Client/Client.ts
+++ src/Client/Client.ts
@@ -14,6 +14,9 @@ import { buildGameFromJSON } from "./GameBuilder.js";
 import { sendClientGamestate } from "../index.js";
 import Auth from "../Components/Auth.js";
 
+/**
+ * Client's state of authentication 
+ */
 type AuthState =
   | {
       isAuthenticated: true;
@@ -28,6 +31,12 @@ type AuthState =
       token: null;
     };
 
+
+/**
+ * Defines the properties of a client.
+ * 
+ * A Client consists of its id, room associated to (if any), and authState. 
+ */
 export default class Client {
     static #nextId : number = 1000;
     identifier: number;
@@ -39,10 +48,21 @@ export default class Client {
         token: null
     };
 
+    /**
+     * Creates a client.
+     */
     constructor()  {
         this.identifier = Client.nextId;
     }
 
+    /**
+     * 
+     * Signs a client into his/her account.
+     * 
+     * @param username - The client's username.
+     * @param password - The clients password.
+     * @returns A token if authentication is successful, otherwise null.
+     */
     async signIn(username: string, password: string) {
         const success = await Auth.authenticateUser(username, password);
         if (!success) return null;
@@ -58,6 +78,13 @@ export default class Client {
         return success.token;
     }
 
+    /**
+     * Creates a new account for the client.
+     * @param username - The client's username.
+     * @param password - The client's password.
+     * @param displayName - The clients name which is displayed to other users.
+     * @returns If successful it returns a sessionId, otherwise null.
+     */
     async signUp(username: string, password: string, displayName: string) {
         const success = await Auth.createNewUser(username, password, displayName);
         if (!success) return null;
@@ -72,6 +99,10 @@ export default class Client {
         return success;
     }
 
+    /**
+     * Signs a client out of his/her account and clears the clients authState.
+     * @returns True if sign out is successful, otherwise false.
+     */
     async signOut() {
         if (!this.isAuthenticated || !this.authState.token) return false;
 
@@ -87,6 +118,12 @@ export default class Client {
         return success;
     }
 
+    /**
+     * Submiting the game rules that the client creates.
+     * @param rules - JSON containing game information and rules.
+     * @returns void, as the resulting game definition isnt used yet.
+     * @todo use the game definition somewhere
+     */
     submitRulesFromEditor(rules: unknown) {
         try {
             const game: GameDefinition | null = buildGameFromJSON(rules);
@@ -98,7 +135,12 @@ export default class Client {
         }
     }
 
-    // TODO: I expect a reference to "Player" or at least "PlayerID" will be stored in the client class eventually
+    /**
+     * Send the updated game state to the client.
+     * @param game - Current game instance.
+     * @param player - The player associated with the client.
+     * @todo I expect a reference to "Player" or at least "PlayerID" will be stored in the client class eventually
+     */
     updateGamestate(game: Game, player: Player) {
         sendClientGamestate(this.identifier, ClientView.fromGamestate(game, player));
     }
