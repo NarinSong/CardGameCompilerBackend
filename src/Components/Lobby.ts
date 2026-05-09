6af6893a959900285634f852241ba683f8e473fa
Coder0987 <ijopkids@gmail.com>
2026-05-08 21:17:19 -0500

    Add some lobby code

--- src/Components/Lobby.ts
+++ src/Components/Lobby.ts
@@ -7,12 +7,45 @@ export default class Lobby {
     #players: Client[];
     #game: string | null;
     #maxPlayers: number = 32;
+    #joinCode: string;
+    #id: number;
 
-    constructor(host: Client) {
+    constructor(host: Client, joinCode: string, id: number) {
         if (!host.isAuthenticated || !host.username) throw new Error("Unauthenticated host created lobby");
 
         this.#host = host.username;
         this.#players = [host];
         this.#game = null;
+        this.#joinCode = joinCode;
+        this.#id = id;
+    }
+
+    joinGame(client: Client) {
+        if (this.#players.length >= this.#maxPlayers) return false;
+
+        this.#players.push(client);
+
+        // todo: set client.lobby to this lobby's id
+        return true;
+    }
+
+    startGame() {
+        if (!this.#game) return false;
+        // Create the room if at least minPlayers (available, e.g. not in game) are in the lobby
+        // Add available players until maxPlayers is reached
+        // Mark those players as "in game"
+        // Send "game started" signals to those players
+    }
+
+    get joinCode() {
+        return this.#joinCode;
+    }
+
+    get availablePlayers() {
+        let s=0;
+        for (let p in this.#players) {
+            //if (!this.#players[p].inGame) s++; // todo add "inGame" flag
+        }
+        return s;
     }
 }
