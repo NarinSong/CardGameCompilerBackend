0d45622df62d3f77ecaa27d11165f673be841f4f
Coder0987 <ijopkids@gmail.com>
2026-06-22 17:07:59 -0500

    Implement BLOCKS

--- src/schemas/Blocks.ts
+++ src/schemas/Blocks.ts
@@ -12,10 +12,20 @@ export const ValueTypes = {
   String: z.string(),
   Boolean: z.boolean(),
   PileLabel: z.string(),
+  ActionRole: z.string(),
   PileState: PileStateSchema,
   Visibility: VisibilitySchema,
   Undefined: z.undefined(),
   Array: z.array(z.any()),
+  Card: z.object({
+    rank: z.string(),
+    value: z.string()
+  }),
+  ID: z.number(),
+  Player: z.number(),
+  PlayerRole: z.string(),
+  Step: z.string(),
+  Phase: z.string(),
 } as const;
 
 export const ValueTypeNameSchema = z.enum(
@@ -206,6 +216,451 @@ const CLICKED_LABEL = defineBlock({
     "arguments": []
 });
 
+const AND = defineBlock({
+    "name": "AND",
+    "displayName": "And",
+    "returnType": "Boolean",
+    "arguments": [
+        {
+            "name": "primary",
+            "displayName": "Left",
+            "type": "Boolean",
+            "optional": false
+        },
+        {
+            "name": "secondary",
+            "displayName": "Right",
+            "type": "Boolean",
+            "optional": false
+        }
+    ]
+});
+
+const OR = defineBlock({
+    "name": "OR",
+    "displayName": "Or",
+    "returnType": "Boolean",
+    "arguments": [
+        {
+            "name": "primary",
+            "displayName": "Left",
+            "type": "Boolean",
+            "optional": false
+        },
+        {
+            "name": "secondary",
+            "displayName": "Right",
+            "type": "Boolean",
+            "optional": false
+        }
+    ]
+});
+
+const NOT = defineBlock({
+    "name": "NOT",
+    "displayName": "Not",
+    "returnType": "Boolean",
+    "arguments": [
+        {
+            "name": "primary",
+            "displayName": "Argument",
+            "type": "Boolean",
+            "optional": false
+        }
+    ]
+});
+
+const PLUS = defineBlock({
+    "name": "PLUS",
+    "displayName": "Plus",
+    "returnType": "Number",
+    "arguments": [
+        {
+            "name": "primary",
+            "displayName": "Left",
+            "type": "Number",
+            "optional": false
+        },
+        {
+            "name": "secondary",
+            "displayName": "Right",
+            "type": "Number",
+            "optional": false
+        }
+    ]
+});
+
+const TIMES = defineBlock({
+    "name": "TIMES",
+    "displayName": "Times",
+    "returnType": "Number",
+    "arguments": [
+        {
+            "name": "primary",
+            "displayName": "Left",
+            "type": "Number",
+            "optional": false
+        },
+        {
+            "name": "secondary",
+            "displayName": "Right",
+            "type": "Number",
+            "optional": false
+        }
+    ]
+});
+
+const MINUS = defineBlock({
+    "name": "MINUS",
+    "displayName": "Minus",
+    "returnType": "Number",
+    "arguments": [
+        {
+            "name": "primary",
+            "displayName": "Left",
+            "type": "Number",
+            "optional": false
+        },
+        {
+            "name": "secondary",
+            "displayName": "Right",
+            "type": "Number",
+            "optional": false
+        }
+    ]
+});
+
+const DIV = defineBlock({
+    "name": "DIV",
+    "displayName": "Divide",
+    "returnType": "Number",
+    "arguments": [
+        {
+            "name": "primary",
+            "displayName": "Left",
+            "type": "Number",
+            "optional": false
+        },
+        {
+            "name": "secondary",
+            "displayName": "Right",
+            "type": "Number",
+            "optional": false
+        }
+    ]
+});
+
+const STRING_EQ = defineBlock({
+    "name": "STRING_EQ",
+    "displayName": "String Equals",
+    "returnType": "Boolean",
+    "arguments": [
+        {
+            "name": "primary",
+            "displayName": "Left",
+            "type": "String",
+            "optional": false
+        },
+        {
+            "name": "secondary",
+            "displayName": "Right",
+            "type": "String",
+            "optional": false
+        }
+    ]
+});
+
+const TERNARY = defineBlock({
+    "name": "TERNARY",
+    "displayName": "Ternary",
+    "returnType": "Number",
+    "arguments": [
+        {
+            "name": "primary",
+            "displayName": "Condition",
+            "type": "Boolean",
+            "optional": false
+        },
+        {
+            "name": "secondary",
+            "displayName": "If true",
+            "type": "Void",
+            "optional": false
+        },
+        {
+            "name": "tertiary",
+            "displayName": "If false",
+            "type": "Void",
+            "optional": false
+        }
+    ]
+});
+
+const CTX_CARD = defineBlock({
+    "name": "CTX_CARD",
+    "displayName": "Passed Card",
+    "returnType": "Card",
+    "arguments": []
+});
+
+const CTX_ID = defineBlock({
+    "name": "CTX_CARD",
+    "displayName": "Clicked Card",
+    "returnType": "ID",
+    "arguments": []
+});
+
+const GET_ID_FROM_ROLE = defineBlock({
+    "name": "GET_ID_FROM_ROLE",
+    "displayName": "Get Player from Role",
+    "returnType": "Player",
+    "arguments": [
+        {
+            "name": "role",
+            "displayName": "Role",
+            "type": "PlayerRole",
+            "optional": false
+        },
+        {
+            "name": "index",
+            "displayName": "Which Player",
+            "type": "Number",
+            "optional": true
+        }
+    ]
+});
+
+const PILE_OF = defineBlock({
+    "name": "PILE_OF",
+    "displayName": "Get a Player's Pile",
+    "returnType": "PileLabel",
+    "arguments": [
+        {
+            "name": "id",
+            "displayName": "Player ID",
+            "type": "Player",
+            "optional": false
+        },
+        {
+            "name": "actionRole",
+            "displayName": "Pile's Action Role",
+            "type": "ActionRole",
+            "optional": false
+        }
+    ]
+});
+
+const HAS_ROLE = defineBlock({
+    "name": "HAS_ROLE",
+    "displayName": "Player has Role",
+    "returnType": "Boolean",
+    "arguments": [
+        {
+            "name": "id",
+            "displayName": "Player ID",
+            "type": "Player",
+            "optional": false
+        },
+        {
+            "name": "role",
+            "displayName": "Player Role",
+            "type": "PlayerRole",
+            "optional": false
+        }
+    ]
+});
+
+const ASSIGN_ROLE = defineBlock({
+    "name": "ASSIGN_ROLE",
+    "displayName": "Assign a Role",
+    "returnType": "Boolean",
+    "arguments": [
+        {
+            "name": "id",
+            "displayName": "Player ID",
+            "type": "Player",
+            "optional": false
+        },
+        {
+            "name": "role",
+            "displayName": "Player Role",
+            "type": "PlayerRole",
+            "optional": false
+        }
+    ]
+});
+
+const UNASSIGN_ROLE = defineBlock({
+    "name": "UNASSIGN_ROLE",
+    "displayName": "Unassign a Role",
+    "returnType": "Boolean",
+    "arguments": [
+        {
+            "name": "id",
+            "displayName": "Player ID",
+            "type": "Player",
+            "optional": false
+        },
+        {
+            "name": "role",
+            "displayName": "Player Role",
+            "type": "PlayerRole",
+            "optional": false
+        }
+    ]
+});
+
+const ASSIGN_ROLE_SINGULAR = defineBlock({
+    "name": "ASSIGN_ROLE_SINGULAR",
+    "displayName": "Assign a Role to One Player",
+    "returnType": "Boolean",
+    "arguments": [
+        {
+            "name": "id",
+            "displayName": "Player ID",
+            "type": "Player",
+            "optional": false
+        },
+        {
+            "name": "role",
+            "displayName": "Player Role",
+            "type": "PlayerRole",
+            "optional": false
+        }
+    ]
+});
+
+const RANK = defineBlock({
+    "name": "RANK",
+    "displayName": "Card Rank",
+    "returnType": "String",
+    "arguments": [
+        {
+            "name": "primary",
+            "displayName": "Card",
+            "type": "Card",
+            "optional": false
+        }
+    ]
+});
+
+const SUIT = defineBlock({
+    "name": "SUIT",
+    "displayName": "Card Suit",
+    "returnType": "String",
+    "arguments": [
+        {
+            "name": "primary",
+            "displayName": "Card",
+            "type": "Card",
+            "optional": false
+        }
+    ]
+});
+
+const MAP = defineBlock({
+    "name": "MAP",
+    "displayName": "Map",
+    "returnType": "Number",
+    "arguments": [
+        {
+            "name": "primary",
+            "displayName": "Index",
+            "type": "String",
+            "optional": false
+        },
+        {
+            "name": "secondary",
+            "displayName": "Map Name",
+            "type": "String",
+            "optional": false
+        },
+    ]
+});
+
+const ADD_VARIABLE = defineBlock({
+    "name": "ADD_VARIABLE",
+    "displayName": "Add Variable",
+    "returnType": "Number",
+    "arguments": [
+        {
+            "name": "name",
+            "displayName": "Variable Name",
+            "type": "String",
+            "optional": false
+        },
+        {
+            "name": "value",
+            "displayName": "Value",
+            "type": "Number",
+            "optional": false
+        },
+    ]
+});
+
+const UPDATE_VARIABLE = defineBlock({
+    "name": "UPDATE_VARIABLE",
+    "displayName": "Update Variable",
+    "returnType": "Number",
+    "arguments": [
+        {
+            "name": "name",
+            "displayName": "Variable Name",
+            "type": "String",
+            "optional": false
+        },
+        {
+            "name": "value",
+            "displayName": "Value",
+            "type": "Number",
+            "optional": false
+        },
+    ]
+});
+
+const GET_VARIABLE = defineBlock({
+    "name": "GET_VARIABLE",
+    "displayName": "Get Variable",
+    "returnType": "Number",
+    "arguments": [
+        {
+            "name": "name",
+            "displayName": "Variable Name",
+            "type": "String",
+            "optional": false
+        },
+    ]
+});
+
+const SET_STEP = defineBlock({
+    "name": "SET_STEP",
+    "displayName": "Set Step",
+    "returnType": "Void",
+    "arguments": [
+        {
+            "name": "primary",
+            "displayName": "Step",
+            "type": "Step",
+            "optional": false
+        },
+    ]
+});
+
+const SET_PHASE = defineBlock({
+    "name": "SET_PHASE",
+    "displayName": "Set Phase",
+    "returnType": "Void",
+    "arguments": [
+        {
+            "name": "primary",
+            "displayName": "Phase",
+            "type": "Phase",
+            "optional": false
+        },
+    ]
+});
+
+
 // Put the blocks together in a registry to export them
 export const BLOCKS = {
     UNDEFINED,
@@ -215,6 +670,31 @@ export const BLOCKS = {
     IF,
     FOR_EACH,
     CLICKED_LABEL,
+    AND,
+    OR,
+    NOT,
+    PLUS,
+    TIMES,
+    MINUS,
+    DIV,
+    STRING_EQ,
+    TERNARY,
+    CTX_CARD,
+    CTX_ID,
+    GET_ID_FROM_ROLE,
+    PILE_OF,
+    HAS_ROLE,
+    ASSIGN_ROLE,
+    UNASSIGN_ROLE,
+    ASSIGN_ROLE_SINGULAR,
+    RANK,
+    SUIT,
+    MAP,
+    ADD_VARIABLE,
+    UPDATE_VARIABLE,
+    GET_VARIABLE,
+    SET_PHASE,
+    SET_STEP,
 } as const;
 
 export type BlockName = keyof typeof BLOCKS;
